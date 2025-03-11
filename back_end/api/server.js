442dac2236b4bfad009f24eee9384f8ad033a539
Marcus <showdeart.mv@gmail.com>
2025-03-11 12:44:33 -0300

    ajustes deploy

--- back_end/api/server.js
+++ back_end/api/server.js
@@ -1,14 +1,23 @@
 import express from "express";
 import cors from "cors";
 import { db } from "./connect.js";
+import path from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 const PORT = 3000;
 
 app.use(cors());
 
-app.get('/', async (request, response) => {
+app.get('/api/', async (request, response) => {
     response.send(await db.collection("Content1").find({}).toArray());
+})
+
+app.use(express.static(path.join(__dirname, "../front_end/dist")));
+
+app.get('*', async (request, response) => {
+    response.sendFile(path.join(__dirname, "../front_end/dist/index.html"));
 })
 
 app.listen(PORT, () => {
